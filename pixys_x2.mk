40e9f58829fc5892e4fb23643183e8f68c83ec18
Sree Ram <sreeramdmech@gmail.com>
2020-04-29 10:16:38 +0530

    x2: Bringup PixysOS

--- pixys_x2.mk
+++ pixys_x2.mk
@@ -7,14 +7,15 @@
 $(call inherit-product, device/realme/x2/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := pixys_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
 PRODUCT_MANUFACTURER := realme
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:9/PKQ1.190630.001/1578551002:user/release-keys"
 
