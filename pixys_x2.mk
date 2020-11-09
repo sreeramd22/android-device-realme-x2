09d530db1056d69b5f8d78e7c5eec1ae15eab69f
Sree Ram <sreeramdmech@gmail.com>
2020-11-09 19:29:23 +0530

    X2: pixys

--- pixys_x2.mk
+++ pixys_x2.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/realme/x2/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common PixysOS stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := xtended_x2
+PRODUCT_NAME := pixys_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
@@ -20,9 +20,3 @@ TARGET_BOOT_ANIMATION_RES := 1080
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1592194586:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
-
-# Xtension
-XTENDED_BUILD_TYPE := OFFICIAL
-
-#Maintainer
-XTENDED_MAINTAINER := SreeRamD
