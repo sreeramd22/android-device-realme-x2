4f4edb260a874a4fc0bdb554f66b0a718d3ba397
Sree Ram <sreeramdmech@gmail.com>
2020-11-11 17:07:53 +0530

    x2: xtended

--- xtended_x2.mk
+++ xtended_x2.mk
@@ -6,13 +6,12 @@
 
 $(call inherit-product, device/realme/x2/device.mk)
 
-# Inherit some common PixysOS stuff.
+# Inherit some common Xtended stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixys_x2
+PRODUCT_NAME := xtended_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
@@ -25,3 +24,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
+
+# Xtension
+XTENDED_BUILD_TYPE := OFFICIAL
+
+#Maintainer
+XTENDED_MAINTAINER := SreeRamD
